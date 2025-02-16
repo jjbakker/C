3cedc799d5b2ace6aa12c8f1dbe60fab76c9ea77
Jaap Jan Bakker <jjbakker@xs4all.nl>
2025-02-16 09:36:47 +0100

    ex17 changes

--- source/ex17.c
+++ source/ex17.c
@@ -73,8 +73,118 @@ void Database_close(struct Connection *conn) {
         if(conn->file) {
             fclose(conn->file);
         }
+    if(conn-db) {
+        free(conn-db);
+    }        
+    
+    free(conn);
+    
+    }
+
+}
+
+
+void Database_write(struct Connection *conn) {
+
+    rewind(conn->file);
+
+    int rc = fwrite(conn->db, sizeof(struct Database),1, conn->file);
+
+    if (rc != 1) {
+        die("Failed to write database.");
+    }
+
+    rc = fflush(conn->file);
+    if (rc == -1) {
+        die("Cannot flush database.");
+    }
+}
+
+void Database_create(struct Connection *conn) {
+
+    int i = 0;
+
+    for (i = 0; i < MAX_ROWS; i++) {
+
+        // make a prototype to initialize it
+        struct Address addr = {.id = i, .set = 0};
+
+        // then just assign it
+        conn->db->rows[i] = addr;
+    }
+}
+
+
+void Database_set(struct Connection *conn, int id, const char *name, const char *email) {
+
+    struct Address *addr = &conn->db->rows[id];
+    if (addr->set) {
+        die("Already set, delete it first");
+    }
+
+    addr->set = 1;
+
+    //warning: bug: read how to break it section
+    char *res = strncpy(addr->name,name, MAX_DATA);
+
+    //demonstrating srncpy bug
+    if(!res) {
+        die("Name copy failed");
+    }
+
+    res = strncpy(addr->email, email, MAX_DATA);
+    if(!res) {
+        die("email copy failed");
+    }
+
+}
+
+
+void Database_get(struct Connection *conn, int id) {
+
+    struct Address *addr = &conn->db->rows[id];
+
+    if(addr->set) {
+        Address_print(addr);
+    } else {
+        die("ID is not set");
+    } 
+}
+
+
+void Database_delete(struct Connection *conn, int id) {
+
+    struct Address addr = {.id = id, .set = 0};
+
+    conn->db->rows[id] = addr;
+ 
+}
+
+void Database_list(struct Connection *conn) {
+
+    int i = 0;
+    struct Database *db = conn->db;
+
+    for(i=0;i<MAX_ROWS;i++) {
+
+        struct Address *cur = &db->rows[i];
+
+        if(cur->set) {
+            Address_print(cur);
+        }
         
     }
+
+}
+
+int main(int argc, char *argv[]) {
+
+    if(argv < 3) {
+        die("USAGE: ex17 <dbfile> <action> [action parameters]");
+    }
+
+
+
 
 }
 
