15438a51df81e217fa79bdc83ec841f7948d2515
Jaap Jan Bakker <jjbakker@xs4all.nl>
2025-02-16 22:22:27 +0100

    ex17 change

--- source/ex17.c
+++ source/ex17.c
@@ -183,8 +183,57 @@ int main(int argc, char *argv[]) {
         die("USAGE: ex17 <dbfile> <action> [action parameters]");
     }
 
+    char *filename = argv[1];
+    char action = argv[2][0];
+    struct Connection *conn = Database_open(filename,action);
+
+    int id = 0;
+
+    if (argc > 3) id = atoi(argv[3]);
+    if (id > MAX_ROWS) die("There is not that many records");
 
 
+    switch (action) {
+
+        case 'c':
+            Database_create(conn);
+            Database_write(conn);
+            break;
+        case 'g':
+            if(argc !=4) 
+                die("Need an id to get");
+            
+            Database_get(conn,id);
+            break;
+
+        case 's':
+            if(argc !=6) 
+                die("Need id, mame, email to set");
+
+            Database_set(conn, id, argv[4], argv[5]);
+            Database_write(con);
+            break;
+        
+        case 'd':
+            if (argc != 4) 
+                die("Need id to delete it");
+            
+            Database_delete(conn, id);
+            Database_write(conn);
+            break;
+
+        case 'l':
+            Database_list(conn);
+            break;
+        
+        default:
+            die("Invalid action. c=create, g=get, s=set, d=del, l=list");
+
+    }
+    
+    Database_close(conn);
+
+    return 0;
 
 }
 
